acd30b1c5bb469a7141dbaf144cf703b97caa4fd
Pratham Waghmare <prathamw922@gmail.com>
2026-07-03 19:07:55 +0530

    Add index.html to root to fix GitHub Pages 404

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Book-Stall-/">
       <AuthProvider>
         <CartProvider>
           <WishlistProvider>
